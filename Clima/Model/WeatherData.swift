39df5658b81d9aac3a574f26c2b8481433b4a52c
deveen <deveenrath@gmail.com>
2020-10-13 18:54:36 +0530

    add weatherData

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by DEVEEN RATNAYAKE on 10/13/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+struct WeatherData:Decodable {
+    let name:String
+    let main:Main
+    let weather:[Weather]
+}
+struct Main:Decodable{
+    let temp: Double
+}
+
+struct Weather:Decodable {
+    let id:Int
+}
